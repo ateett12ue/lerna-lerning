53ddad2a9c6ef3dbb26bc02256abd91948dc6b84
Ateet Tiwari <ateettiwari@Ateets-MacBook-Pro.local>
2020-02-04 13:11:34 +0530

    initial set ups

---
+++ packages/coconut/index.js
@@ -0,0 +1,6 @@
+const sillyname = require('sillyname');
+const apple = require('apple');
+const banana = require('banana');
+console.log(`coconut and ${sillyname()}`);
+console.log(apple);
+console.log(banana);
